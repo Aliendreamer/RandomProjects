a3e54f8a9f4173e4a8310fcfdf29710860fdc464
Teodor Stefanov <dreamingman83@gmail.com>
2019-04-21 22:43:02 +0300

    algo course added

---
+++ AlgoCourse/RecursionProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace RecursionProblems
+{
+	using System;
+	using System.Linq;
+
+	class Program
+	{
+		static void Main()
+		{
+			var list = Enumerable.Range(0, 500).ToArray();
+			
+			var r=new Recursion<int>(list);
+			var reversed = r.ReverseCollection();
+
+			Console.WriteLine(string.Join(" ",reversed));
+		}
+	}
+}
---
+++ AlgoCourse/RecursionProblems/Recursion.cs
@@ -0,0 +1,75 @@
+﻿namespace RecursionProblems
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	public class Recursion<T>
+	{
+		private T[] Collection { get; set; }
+		private int Index {get;set;}
+
+		public Recursion()
+		{
+				
+		}
+
+		public Recursion(int index)
+		{
+			this.Index = index;
+		}
+		public Recursion(T[] arr)
+		{
+			this.Collection =arr;
+		}
+
+		public void GetCollection(T[] arr)
+		{
+			this.Collection = arr;
+		}
+
+
+		public void GetIndex(int index)
+		{
+			this.Index = index;
+		}
+
+		public IEnumerable<T> ReverseCollection()
+		{
+			if (!this.Collection.Any())
+			{
+				throw new ArgumentException("the massive shouldn't be empty!");
+			}
+			var currentIndex = 0;
+			
+			this.Collection = this.Collection.OrderBy(x => x).ToArray();
+			var middlePoint = this.Collection.Count() / 2;
+			var collectionReversed= ShiftRecursively(currentIndex, middlePoint);
+			return collectionReversed;
+		}
+
+		private IEnumerable<T> ShiftRecursively(int currentIndex,int middlePoint)
+		{
+		
+			var current = this.Collection[currentIndex];
+			var indexToSwap = (this.Collection.Length-1) - currentIndex;
+			var elementToChange = this.Collection[indexToSwap];
+			var holder = current;
+			
+			this.Collection[currentIndex] = elementToChange;
+			this.Collection[indexToSwap] = holder;
+		
+			if (currentIndex < middlePoint)
+			{
+				ShiftRecursively(currentIndex + 1, middlePoint);
+			}
+			return this.Collection;
+		}
+
+
+		public void NestedLoopsRecursion(int n)
+		{
+			
+		}
+	}
+}
