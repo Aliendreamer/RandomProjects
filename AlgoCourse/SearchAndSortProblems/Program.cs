fbf4b67206a10214b62b05e8b355f1345c18834f
Aliendreamer <dreamingman83@gmail.com>
2019-04-28 21:01:52 +0300

    2 of the sorting algorithms problems finished the two harder tommorow

---
+++ AlgoCourse/SearchAndSortProblems/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SearchAndSortProblems
+{
+	using System;
+	using System.Linq;
+
+	internal class Program
+	{
+		private static void Main()
+		{
+			var sequence = Console.ReadLine()?.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+			var num = int.Parse(Console.ReadLine());
+			var baseS = new SortBase();
+			var index = baseS.GetElementWithBinarySearch(sequence, num);
+			Console.WriteLine(index);
+			//baseS.SortedSequenceWithBubleSort(sequence);
+			//baseS.PrintCollection();
+		}
+	}
+}
---
+++ AlgoCourse/SearchAndSortProblems/SortBase.cs
@@ -0,0 +1,50 @@
+﻿namespace SearchAndSortProblems
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class SortBase
+	{
+		private IList<int> SortedCollection { get; set; }
+
+		public IEnumerable<int> SortedSequenceWithBubleSort(int[] array)
+		{
+			this.SortedCollection = new List<int>(array);
+			int index = 0;
+			bool swapped = true;
+
+			while (swapped)
+			{
+				swapped = false;
+				for (int i = 0; i < this.SortedCollection.Count - 1; i++)
+				{
+					var current = SortedCollection[i];
+					var next = SortedCollection[i + 1];
+					if (current > next)
+					{
+						SortedCollection[i] = next;
+						SortedCollection[i + 1] = current;
+						swapped = true;
+					}
+				}
+			}
+
+			return this.SortedCollection;
+		}
+
+		public void PrintCollection()
+		{
+			Console.WriteLine(string.Join(" ", this.SortedCollection));
+		}
+
+		public int GetElementWithBinarySearch(int[] arr, int num)
+		{
+			var result = Array.BinarySearch(arr, 0, arr.Length, num);
+			if (result < 0)
+			{
+				result = -1;
+			}
+			return result;
+		}
+	}
+}
