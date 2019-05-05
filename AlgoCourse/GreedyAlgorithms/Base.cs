f776d231b74b2cb1b4d82d19789254c38c3d746e
Aliendreamer <dreamingman83@gmail.com>
2019-05-05 11:03:34 +0300

    greedy algo finished

--- AlgoCourse/GreedyAlgorithms/Base.cs
+++ AlgoCourse/GreedyAlgorithms/Base.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GreedyAlgorithms
+﻿namespace GreedyAlgorithms
 {
-	using System.Data;
+	using System;
+	using System.Collections.Generic;
 	using System.Linq;
 
 	public class Base
@@ -118,36 +115,145 @@ namespace GreedyAlgorithms
 
 		public static void KnightProblem()
 		{
-			var size = int.Parse(Console.ReadLine());
+			int count = int.Parse(Console.ReadLine());
+			List<Cell> board = new List<Cell>();
 
-			var board = new int[size, size];
-
-			//fill matrix
-			for (int row = 0; row < board.GetLength(0); row++)
+			for (byte i = 1; i <= count; i++)
 			{
-				for (int col = 0; col < board.GetLength(1); col++)
+				for (byte j = 1; j <= count; j++)
 				{
-					if (col == 0 && row == 0)
-					{
-						board[row, col] = 1;
-					}
-					board[row, col] = 0;
+					board.Add(new Cell { Row = i, Col = j });
 				}
 			}
+			int counter = 1;
+			var currentCell = board[0];
+			currentCell.IsVisited = true;
+			currentCell.TurnVisited = counter++;
+
+			while (board.Any(c => !c.IsVisited))
+			{
+				currentCell = SelectNextCell(currentCell, board);
+				currentCell.IsVisited = true;
+				currentCell.TurnVisited = counter++;
+			}
+
+			PrintBoard(count, board);
+		}
+
+		public static void EgyptianFractions()
+		{
+			// ReSharper disable once PossibleNullReferenceException
+			int[] tokens = Console.ReadLine().Split('/').Select(int.Parse).ToArray();
+			int p = tokens[0];
+			int q = tokens[1];
+
+			if (p >= q)
+			{
+				Console.WriteLine("Error (fraction is equal to or greater than 1)");
+				return;
+			}
+
+			Console.Write($"{p}/{q} = ");
+			if (q % p == 0)
+			{
+				q = q / p;
+				p = 1;
+				Console.WriteLine($"1/{q}");
+				return;
+			}
+
+			while (true)
+			{
+				int divider = (p + q) / p;
+				Console.Write($"1/{divider} + ");
+
+				p = (p * divider) - q;
+				q = q * divider;
+
+				if (q % p != 0) continue;
+				q = q / p;
+				p = 1;
+				Console.WriteLine($"1/{q}");
+				break;
+			}
+		}
+
+		private static void PrintBoard(int count, List<Cell> board)
+		{
+			for (int i = 0; i < count; i++)
+			{
+				for (int j = 0; j < count; j++)
+				{
+					Console.Write(board[i * count + j].TurnVisited.ToString().PadLeft(3) + " ");
+				}
+				Console.WriteLine();
+			}
+		}
+
+		private static Cell SelectNextCell(Cell current, List<Cell> board)
+		{
+			var topLeft = board.FirstOrDefault(c => c.Row == current.Row - 2 && c.Col == current.Col - 1);
+			var leftTop = board.FirstOrDefault(c => c.Row == current.Row - 1 && c.Col == current.Col - 2);
+			var rightTop = board.FirstOrDefault(c => c.Row == current.Row - 1 && c.Col == current.Col + 2);
+			var topRight = board.FirstOrDefault(c => c.Row == current.Row - 2 && c.Col == current.Col + 1);
+
+			var bottomLeft = board.FirstOrDefault(c => c.Row == current.Row + 2 && c.Col == current.Col - 1);
+			var leftBottom = board.FirstOrDefault(c => c.Row == current.Row + 1 && c.Col == current.Col - 2);
+			var rightBottom = board.FirstOrDefault(c => c.Row == current.Row + 1 && c.Col == current.Col + 2);
+			var bottomRight = board.FirstOrDefault(c => c.Row == current.Row + 2 && c.Col == current.Col + 1);
+
+			return new List<Cell>
+				{
+			 rightBottom,rightTop,leftBottom, leftTop, bottomRight, topRight, topLeft, bottomLeft
+				}
+			.Where(c => c != null && !c.IsVisited)
+			.ToList()
+			.OrderBy(c => CalculatePosibleMoves(c, board))
+			.First();
+		}
+
+		private static int CalculatePosibleMoves(Cell current, List<Cell> board)
+		{
+			var topLeft = board.FirstOrDefault(c => c.Row == current.Row - 2 && c.Col == current.Col - 1);
+			var leftTop = board.FirstOrDefault(c => c.Row == current.Row - 1 && c.Col == current.Col - 2);
+			var rightTop = board.FirstOrDefault(c => c.Row == current.Row - 1 && c.Col == current.Col + 2);
+			var topRight = board.FirstOrDefault(c => c.Row == current.Row - 2 && c.Col == current.Col + 1);
+
+			var bottomLeft = board.FirstOrDefault(c => c.Row == current.Row + 2 && c.Col == current.Col - 1);
+			var leftBottom = board.FirstOrDefault(c => c.Row == current.Row + 1 && c.Col == current.Col - 2);
+			var rightBottom = board.FirstOrDefault(c => c.Row == current.Row + 1 && c.Col == current.Col + 2);
+			var bottomRight = board.FirstOrDefault(c => c.Row == current.Row + 2 && c.Col == current.Col + 1);
+
+			return new List<Cell>
+					{
+						topLeft, topRight, leftTop, rightTop, bottomLeft, bottomRight, leftBottom, rightBottom
+					}
+			.Count(c => c != null && !c.IsVisited);
 		}
 	}
+}
 
-	public class ScheduleTask
-	{
-		public int Value { get; set; }
+public class Cell
+{
+	public byte Row { get; set; }
 
-		public int Step { get; set; }
-	}
+	public byte Col { get; set; }
 
-	public class Item
-	{
-		public int Weight { get; set; }
+	public bool IsVisited { get; set; }
 
-		public int Price { get; set; }
-	}
+	public int TurnVisited { get; set; }
+}
+
+public class ScheduleTask
+{
+	public int Value { get; set; }
+
+	public int Step { get; set; }
+}
+
+public class Item
+{
+	public int Weight { get; set; }
+
+	public int Price { get; set; }
 }
