627781f3a51211fdaa2a43e33e9a7a23ed7e1b81
Teodor Stefanov <dreamingman83@gmail.com>
2019-06-16 19:44:03 +0300

    pretty nice little project iwth most well known ds and algos

---
+++ AlgoAndDsProject/AlgoAndDsProject/AlgoAndDsProject/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AlgoAndDsProject
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using Algorithms.Sorting;
+	using DataStructures.Trees;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		     var list = Enumerable.Range(0, 1000).ToList();
+		//	 list.BubbleSortAscending(Comparer<int>.Default);
+		//	 Console.WriteLine(string.Join($"{Environment.NewLine}",list));
+
+			var tree = new RedBlackTree<int>();
+			foreach (var i in list)
+			{
+				tree.Insert(i);
+			}
+
+			Console.WriteLine(tree.DrawTree());
+		}
+	}
+}
